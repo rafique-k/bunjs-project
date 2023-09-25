d94c042b305d65c98c74b876295508bcfaf572e1
Rafique Khan <rafique@carrot.club>
2023-09-25 14:50:59 +0530

    done few changes in server

--- index.ts
+++ index.ts
@@ -4,8 +4,9 @@ const server = Bun.serve({
     port: 3000,
     fetch(req)
     {
-        return new Response("Bun is running successfully..!")
-    }
+        return new Response("Bun is running successfully on ports ..!")
+        // console.log("This is another line of code i have written");
+    },
 });
 console.log(`Bun Server is running at http://localhost:${server.port}`);
 console.log("This is another line of code i have written");
