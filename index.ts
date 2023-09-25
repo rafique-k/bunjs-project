98ba33e1281a1c745c5ac554c86290db8ff30fe2
Rafique Khan <rafique@carrot.club>
2023-09-25 14:38:51 +0530

    created bun server

--- index.ts
+++ index.ts
@@ -1,2 +1,11 @@
 console.log("Hello via Bun!");
+
+const server = Bun.serve({
+    port: 3000,
+    fetch(req)
+    {
+        return new Response("Bun is running successfully..!")
+    }
+});
+console.log(`Bun Server is running at http://localhost:${server.port}`);
 console.log("This is another line of code i have written");
