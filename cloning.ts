3605e26437cda51493048e443c5013a1dcb60b73
Rafique Khan <rafique@carrot.club>
2023-10-11 12:39:16 +0530

    updated cloning.ts

--- cloning.ts
+++ cloning.ts
@@ -24,23 +24,48 @@ console.log(shallowCopy);
 // let availability = {isAvailable: true};
 // Object.assign(originalRecipeBook,availability);
 shallowCopy.recipe[0].ingredients.unshift("Sugar");
+
+console.log("----------- Sugar added in shallowCopy Updated Result-----------")
+console.log(shallowCopy);
+console.log("-----------Original Book Result After Sugar added in Shallow copy-----------")
+console.log(originalRecipeBook);
+
 originalRecipeBook.recipe[1].ingredients.unshift("Chicken");
 
-console.log("----------- shallowCopy Updated Result-----------")
-console.log(shallowCopy);
-
-console.log("----------- originalRecipeBook After Updated Result-----------")
+console.log("----------- Chicken Added in originalRecipeBook After Updated Result-----------")
 console.log(originalRecipeBook);
 
-const deepCopy = _.clone(originalRecipeBook);
+console.log("----------- shallowCopy After Chicken Added Result-----------")
+console.log(shallowCopy);
+// const deepCopy = _.clone(originalRecipeBook);
+// const deepCopy = structuredClone(originalRecipeBook);
+const deepCopy = JSON.parse(JSON.stringify(originalRecipeBook));
 
 console.log("----------- deepCopy Result-----------")
-console.log(shallowCopy);
+console.log(deepCopy);
 
-deepCopy.recipe[1].ingredients.unshift("Cooking Oil");
+deepCopy.recipe[0].ingredients.unshift("CookingOil");
 
-console.log("----------- Updated deepCopy Result-----------")
-console.log(shallowCopy);
+console.log("----------- deepCopy After Adding Cooking Oil Result-----------")
+console.log(deepCopy);
 
-console.log("----------- originalRecipeBook After DeepCopy Updated Result-----------")
+console.log("----------- originalRecipeBook After Adding Cooking Oil in Deep Copy-----------")
 console.log(originalRecipeBook);
+
+console.log("----------- shallowCopy After Adding Cooking Oil in Deep Copy-----------")
+console.log(shallowCopy);
+// deepCopy.recipe[1].ingredients.unshift("Cooking Oil");
+
+// console.log("----------- Updated deepCopy Result-----------")
+// console.log(shallowCopy);
+
+// console.log("----------- originalRecipeBook After DeepCopy Updated Result-----------")
+// console.log(originalRecipeBook);
+
+originalRecipeBook.recipe[1].ingredients.unshift("Paneer");
+
+console.log("----------- Paneer Added in originalRecipeBook After Updated Result-----------")
+console.log(originalRecipeBook);
+
+console.log("----------- deepCopy After Adding Paneer Result-----------")
+console.log(deepCopy);
